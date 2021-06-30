d9da8ce84a5633a80d0393ed9660edcdab7b63fa
anubhuti25 <anubhuti.prabha@gmail.com>
2021-06-30 22:56:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,73 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const url = require('url');
+const shortid = require('shortid');
+
+const { Schema } = mongoose; 
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: String
+});
+
+let URL = mongoose.model('URL', urlSchema);
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const short_url  = req.params.short_url;
+  URL.findOne({short_url: short_url}, (err,data) => {
+    if(err) return res.status(500).send({msg: err.message});
+    if(data) {
+      return res.redirect(data.original_url)
+    } else {
+      res.status(404).json({ error: 'No URL found' })
+    }
+  });
+});
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  const original_url = req.body.url;
+  dns.lookup(url.parse(original_url).hostname, (err, address, family) => {
+    if(err) {
+      res.json({ error: 'invalid url' })
+    } else {
+      
+        URL.findOne({original_url}, (err,data) => {
+          if(err) return res.status(500).send({msg: err.message});
+          if(data) {
+            return res.json({original_url : data.original_url,short_url: data.short_url})
+          } 
+          const url = new URL({
+            original_url: original_url,
+            short_url: shortid.generate()
+          });
+          url.save((err, data) => {
+            if (err) return res.status(500).send({ msg: err.message });
+            res.json({ original_url: url.original_url, short_url: url.short_url });
+          });
+        })
+    }
+  });
 });
 
 app.listen(port, function() {
